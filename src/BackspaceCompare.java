d146fdd2a3910270034746554b3533e4a0a80ad3
hjs <819705323@qq.com>
2020-10-19 12:25:36 +0800

    20201019

--- src/BackspaceCompare.java
+++ src/BackspaceCompare.java
@@ -1,1 +1,30 @@
-/** * @author hjs */public class BackspaceCompare {    public boolean compare(String S, String T) {        return build(S).equals(build(T));    }    public String build(String s) {        StringBuffer ret = new StringBuffer();        int length = s.length();        for (int i = 0; i < length; ++i) {            char ch = s.charAt(i);            if (ch != '#') {                ret.append(ch);            } else {                if (ret.length() > 0) {                    ret.deleteCharAt(ret.length() - 1);                }            }        }        return ret.toString();    }    public static void main(String[] args) {        BackspaceCompare test = new BackspaceCompare();        System.out.println(test.compare("ab#c", "ad#c"));    }}
+/**
+ * @author hjs
+ */
+public class BackspaceCompare {
+    public boolean compare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String s) {
+        StringBuffer ret = new StringBuffer();
+        int length = s.length();
+        for (int i = 0; i < length; ++i) {
+            char ch = s.charAt(i);
+            if (ch != '#') {
+                ret.append(ch);
+            } else {
+                if (ret.length() > 0) {
+                    ret.deleteCharAt(ret.length() - 1);
+                }
+            }
+        }
+        return ret.toString();
+    }
+
+    public static void main(String[] args) {
+        BackspaceCompare test = new BackspaceCompare();
+        System.out.println(test.compare("ab#c", "ad#c"));
+    }
+
+}
