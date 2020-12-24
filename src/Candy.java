5c61d34f92c575744a3c09cf3d3f108f87d74557
hjs <819705323@qq.com>
2020-12-24 19:18:06 +0800

    20201224:Christmas Eve

--- src/Candy.java
+++ src/Candy.java
@@ -1,7 +1,29 @@
 public class Candy {
-    int ratings;
-    public int candy(int[] ratings){
+        public int candy(int[] ratings) {
+            int n = ratings.length;
+            int[] left = new int[n];
+            for (int i = 0; i < n; i++) {
+                if (i > 0 && ratings[i] > ratings[i - 1]) {
+                    left[i] = left[i - 1] + 1;
+                } else {
+                    left[i] = 1;
+                }
+            }
+            int right = 0, ret = 0;
+            for (int i = n - 1; i >= 0; i--) {
+                if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+                    right++;
+                } else {
+                    right = 1;
+                }
+                ret += Math.max(left[i], right);
+            }
+            return ret;
+        }
 
-        return 0;
+    public static void main(String[] args) {
+        Candy test=new Candy();
+        int []a={1,2,2};
+        System.out.println(test.candy(a));
     }
 }
