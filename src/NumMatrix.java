2dc522d5a4bb6576f4ad9bfbb66a865e45717c19
hjs <819705323@qq.com>
2021-03-02 13:16:26 +1100

    20210302

---
+++ src/NumMatrix.java
@@ -0,0 +1,28 @@
+public class NumMatrix {
+    int[][] sums;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            sums = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
+    }
+
+    public static void main(String[] args) {
+        int[][] a={{3,0,1,4,2},{5,6,3,2,1},{1,2,0,1,5},{4,1,0,1,7},{1,0,3,0,5}};
+        NumMatrix test=new NumMatrix(a);
+        System.out.println(test.sumRegion(2,1,4,3));
+        System.out.println(test.sumRegion(1,1,2,2));
+        System.out.println(test.sumRegion(1,2,2,4));
+    }
+}
